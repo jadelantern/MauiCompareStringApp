7b9ab914eed8ff393bcbe9b692d58a5b8a1198b5
nate lee <natelee00@outlook.com>
2022-12-28 21:49:58 -0500

    Add project files.

---
+++ MauiCompareStringApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiCompareStringApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiCompareStringApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiCompareStringApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
